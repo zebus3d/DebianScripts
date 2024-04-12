3fb6e05293829d0e051af6d9b45103ba5a8c290f
zebus3d <zebus3dream@gmail.com>
2024-04-13 00:19:34 +0200

    instalador de vsc

--- Testing/replace_pulseaudio_with_pipewire.sh
+++ Testing/replace_pulseaudio_with_pipewire.sh
@@ -8,3 +8,5 @@
 pulseaudio --kill
 sudo apt purge pulseaudio
 sudo apt install -y pipewire pipewire-audio-client-libraries pulseaudio-utils wireplumber libspa-0.2-bluetooth
+systemctl --user restart wireplumber pipewire pipewire-pulse
+systemctl --user --now enable wireplumber.service
