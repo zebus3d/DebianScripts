c20bd3eb091335e6eaf2bbc6bdbf1609b6e43f31
zebus3d <zebus3dream@gmail.com>
2024-07-27 21:06:00 +0200

    cambios

--- Stable/install_kde_plasma.py
+++ Stable/install_kde_plasma.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# Esto instala algo parecido al meta paquete kde-plasma-desktop pero sin konqueror
+
 sudo apt install plasma-desktop plasma-workspace dolphin kate kdialog kfind konsole udisks2 upower kwin-x11 sddm xserver-xorg ark gwenview kcalc kde-spectacle knotes kwalletmanager okular plasma-dataengines-addons plasma-pa plasma-runners-addons plasma-wallpapers-addons plasma-widgets-addons polkit-kde-agent-1 sweeper plasma-nm skanlite apper print-manager fonts-symbola iw x11-apps x11-session-utils xinit xorg
