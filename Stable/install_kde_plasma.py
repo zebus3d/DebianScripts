3b7602458d54775135d98dc1831ad2818635ebaf
zebus3d <zebus3dream@gmail.com>
2024-07-27 21:04:08 +0200

    cambios

---
+++ Stable/install_kde_plasma.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt install plasma-desktop plasma-workspace dolphin kate kdialog kfind konsole udisks2 upower kwin-x11 sddm xserver-xorg ark gwenview kcalc kde-spectacle knotes kwalletmanager okular plasma-dataengines-addons plasma-pa plasma-runners-addons plasma-wallpapers-addons plasma-widgets-addons polkit-kde-agent-1 sweeper plasma-nm skanlite apper print-manager fonts-symbola iw x11-apps x11-session-utils xinit xorg
